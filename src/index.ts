e9b6afe754e0dd346d41c5aba0f06f943e40c863
Jason Alderman <jason.alderman@mjdinteractive.com>
2020-04-13 11:30:13 -0700

    Made index.ts a webserver that responds with 'hello world'.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello, world!");
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  // no matter what endpoint is hit, return "hello, world!"
+  res.writeHead( 200, { "Content-Type": "text/html" });
+  res.write(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Hello, world.</title>
+  </head>
+  <body>
+    <h1>Hello, world.</h1>
+  </body>
+</html>`);
+  res.end();
+});
+
+server.listen(5000);
+
+console.log("Node.js webserver is listening on localhost:5000...");
